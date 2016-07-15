b5fb5a347bf6dd6f1ce4587d8329d0f613cfe9ca
Christian Syska <belua85@gmail.com>
2016-07-15 18:01:46 +0200

    Cleanup
    Some cleaning up, moving stuff around and writing some comments to
    remind myself what to do next.

--- MainBot.js
+++ MainBot.js
@@ -1,4 +1,3 @@
-var elements;
 var cookieClicking;
 var clickSpeed = 100;
 var autoBuyBuildings;
@@ -8,7 +7,7 @@ var autoClickGolden;
 var autoClickSeason;
 
 function initialize() {
-	clickSpeed = 100;
+	setClicksPerSecond(60);
 	autoBuyBuildings = false;
 	autoBuyUpgrades = false;
 	considerLuckyBonus = false;
@@ -18,25 +17,15 @@ function initialize() {
 	//TODO: add buttons for auto buying and auto clicking
 }
 
-function getColor(element) {
-	var rgb = element.style.color;
-
-	rgb = rgb.substring(4, rgb.length-1)
-			 .replace(/ /g, '')
-			 .split(',');
-	return rgb;
-}
-
-function clickBest() {
+function clickBestBuilding() {
 	var minPP = Infinity;
 	var index;
-	for (var o in CM.Cache.Objects) {
+	for (var o in CM.Cache.Objects) { //It took me an embarrasingly long time to realize that CM.Cache.Objects is, in fact, an object, not an array, and that array.map would not get me anywhere. I blame the Firefox console for telling blatantly lying and telling me it was an array. </rant>
 		if (CM.Cache.Objects[o].pp < minPP) {
 			minPP = CM.Cache.Objects[o].pp;
 			index = o;
 		}
 	}
-	var obj = Game.Objects[index];
 	var poor = true;
 	if (Game.Objects[index].price < Game.cookies) {
 		Game.Objects[index].buy();
@@ -47,18 +36,10 @@ function clickBest() {
 		if (poor) {
 			time = 10000; //If we're poor, we should save up a bit instead of trying again in half a second.
 		}
-		setTimeout(clickBest, time);
+		setTimeout(clickBestBuilding, time);
 	}
 }
 
-function startBuyingBuildings() {
-	autoBuyBuildings = true;
-	clickBest();
-}
-
-function stopBuyingBuildings() {
-	autoBuyBuildings = false;
-}
 function clickGold() {
 	if (goldenCookie.style.display != "none") {
 		goldenCookie.click();
@@ -67,8 +48,9 @@ function clickGold() {
 		setTimeout(clickGold, 1500);
 	}
 }
+
 function buyUpgrades() {
-	var myUpgrades = document.getElementById('upgrades').getElementsByClassName('upgrade enabled');
+	var myUpgrades = document.getElementById('upgrades').getElementsByClassName('upgrade enabled'); //TODO: Holy shit, I hope nobody sees this. What was I thinking? Replace with actual Game.UpgradesInStore.
 	if (myUpgrades.length > 0) {
 		myUpgrades[0].click();
 		setTimeout(buyUpgrades, 500);
@@ -77,14 +59,24 @@ function buyUpgrades() {
 }
 
 function setClicksPerSecond(number) {
-	clickSpeed = 1000/number;
+	clickSpeed = 1000/number; //TODO: Why doesn't the clickspeed work as intended?
 }
 
-function clickCookie() {
+function clickCookie() { //Either redo this or redo the other setTimeouts, so that they're all somewhat consistent.
 	Game.ClickCookie();
-	cookieClicking = setTimeout(clickCookie, clickSpeed);
+	cookieClicking = setTimeout(clickCookie, clickSpeed); //TODO: Why doesn't the clickspeed work as intended? I get that it's not 100% accurate, but the Average Cookie Clicks Per Second are all over the place, sometimes decreasing when they should be increasing, or the other way around. While I was typing this comment, I've seen them go from 40 to 18 and back to 38 without anything even being changed.
 }
 
+function clickSeason() {
+	if (seasonPopup.style.display != "none") {
+		seasonPopup.click();
+	}
+	if (autoClickSeason === true) {
+		setTimeout(clickSeason, 5000); //TODO: this only works if they stay long enough, after buying the upgrades - else they might escape. I should check what upgrades are owned and adjust the time accordingly.
+	}
+}
+
+//TODO: merge this start/stop mess into single toggleX() functions, so that I may hold on to my sanity a tiny bit longer
 function startClicking() {
 	cookieClicking = setTimeout(clickCookie, clickSpeed);
 }
@@ -93,22 +85,22 @@ function stopClicking() {
 	clearTimeout(cookieClicking);
 }
 
+function startBuyingBuildings() {
+	autoBuyBuildings = true;
+	clickBestBuilding();
+}
+
+function stopBuyingBuildings() {
+	autoBuyBuildings = false; //TODO: make timeout variable and clear it to prevent potentially unwanted "one last execution" syndrome?
+}
+
 function startAutoClickGold() {
 	autoClickGolden = true;
 	clickGold();
 }
 
 function stopAutoClickGold() {
-	autoClickGolden = false; //TODO: make timeout variable and clear it?
-}
-
-function clickSeason() {
-	if (seasonPopup.style.display != "none") {
-		seasonPopup.click();
-	}
-	if (autoClickSeason === true) {
-		setTimeout(clickSeason, 5000);
-	}
+	autoClickGolden = false; //TODO: make timeout variable and clear it to prevent potentially unwanted "one last execution" syndrome?
 }
 
 function startAutoClickSeason() {
@@ -117,5 +109,5 @@ function startAutoClickSeason() {
 }
 
 function stopAutoClickSeason() {
-	autoClickSeason = false; //TODO: make timeout variable and clear it?
+	autoClickSeason = false; //TODO: make timeout variable and clear it to prevent potentially unwanted "one last execution" syndrome?
 }
